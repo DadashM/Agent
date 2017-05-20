4e9624c0ba25a2621be6ebe3df605094d8bed1ef
Dadash Mammadli - HOME <dadashmemmedli@hotmail.com>
2017-05-21 01:59:04 +0400

    Exception must catched

--- src/TableSortingAction.java
+++ src/TableSortingAction.java
@@ -1,6 +1,7 @@
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Scanner;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableRowSorter;
 
@@ -12,34 +13,27 @@ public class TableSortingAction {
         defaultTableModel = (DefaultTableModel) gui.tableOfHosts.getModel();
         TableRowSorter<DefaultTableModel> rowSorter = new TableRowSorter<DefaultTableModel>(defaultTableModel);
 
-        rowSorter.setComparator(0, new IntCompare());
+        rowSorter.setComparator(0, new iPSorting());
         gui.tableOfHosts.setRowSorter(rowSorter);
         gui.tableOfHosts.setAutoCreateRowSorter(false);
     }
 
 }
 
-class IntCompare implements Comparator {
+class iPSorting implements Comparator<String> {
 
-    @Override
-    public int compare(Object o1, Object o2) {
+    static Long toNumeric(String ip) {
 
-        Integer int1 = (Integer) intO1(o1);
-        Integer int2 = (Integer) o2;
+        Scanner sc = new Scanner(ip).useDelimiter("\\.");
+        Long l = (sc.nextLong() << 24) + (sc.nextLong() << 16) + (sc.nextLong() << 8) + (sc.nextLong());
+        sc.close();
+        return l;
 
-        return int1.compareTo(int2);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        return this.equals(obj);
-    }
-
-    public void intO1(Object o1) {
-        String str1 = (String) o1;
-        String[] final1 = str1.split("\\.");
-
-        System.out.println(Arrays.toString(final1));
+    public int compare(String o1, String o2) {
+        return toNumeric(o1).compareTo(toNumeric(o2));
     }
 
 }
